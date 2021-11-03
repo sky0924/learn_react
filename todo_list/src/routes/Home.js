3f35adc655f40d201cb13c3f64447441f1b0405a
sky <sbd0924@gmail.com>
2021-11-03 18:58:23 +0900

    update default value and css at todo-item

--- todo_list/src/routes/Home.js
+++ todo_list/src/routes/Home.js
@@ -9,17 +9,20 @@ const colors = ['#000000','#1d3557', '#6b705c', '#ff6b6b'];
 class App extends Component {
 
   // 🤔? id에 초기값을 넣어주는 걸까?
-  id = 6
+  id = 7
 
   state = {
     input: '',
     todos: [
-      { id: 0, text: '할 일 1. Redux 이해하기', checked: false },
+      { id: 0, text: '할 일 1. Redux 이해 및 적용하기', checked: false },
       { id: 1, text: '할 일 2. react-router-dom 이용하기', checked: true },
       { id: 2, text: '할 일 3. image upload 기능 구현하기', checked: false },
       { id: 3, text: '할 일 4. color 추가 및 삭제 기능 구현하기', checked: false },
       { id: 4, text: '할 일 5. user id 및 password 입력 form 만들기', checked: false },
-      { id: 5, text: '할 일 6. 두 줄이 넘어가면 어떻게 되는지 테스트할 아주 길다랗고 길다란 문장, 보기에 나쁘진 않은데 check-mark가 toggle 전환될 때마다 width가 변화함', checked: false }
+      { id: 5, 
+        text: '할 일 6. 두 줄이 넘어가면 어떻게 되는지 테스트할 아주 길다랗고 길다란 문장, 크게 나쁘진 않은데 check-mark가 toggle 전환될 때마다 items의 width가 변화함', 
+        checked: true },
+      { id: 6, text: '할 일 7. GitHub Page 만들기', checked: true }
     ],
     color: ''
   }
