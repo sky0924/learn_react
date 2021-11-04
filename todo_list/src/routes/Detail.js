a21ece550b9ab5def0fa5e9fa1d65c97378ef3f8
sky <sbd0924@gmail.com>
2021-11-04 17:18:35 +0900

    update sub pages

--- todo_list/src/routes/Detail.js
+++ todo_list/src/routes/Detail.js
@@ -6,15 +6,21 @@ import './Detail.css';
 class Detail extends Component {
 
   render() {
+
     return (
       <main className="detail">
         <Link className="detail-back" to="/"><IoArrowBackCircle /></Link>
         <div className="detail-title">
-          할 일 1. Redux 이해하기
+          {/* { text } */}
+          할 일 1. Redux 이해 및 적용하기
         </div>
         <section className="detail-content">
           <div className="detail-textarea">
-            <textarea placeholder="내용을 입력하세요." />
+            <textarea placeholder="내용을 입력하세요." value="Redux란? &#13;- Redux는 오픈 소스 자바스크립트 라이브러리이다. &#13;- state를 이용한 웹 사이트 혹은 애플리케이션의 상태 관리를 목적으로 사용한다." />
+          </div>
+          <div className="detail-buttons">
+            <button>취소</button>
+            <button>저장</button>
           </div>
         </section>
       </main>
--- todo_list/src/routes/User.js
+++ todo_list/src/routes/User.js
@@ -9,16 +9,54 @@ class User extends Component {
     return (
       <main className="user">
         <Link className="user-back" to="/"><IoArrowBackCircle /></Link>
+
         <div className="user-title">
           나의 정보
+
+          <div className="user-buttons">
+            {/* <button>회원가입</button> */}
+            <button>로그인</button>
+            {/* <button>로그아웃</button> */}
+          </div>
         </div>
+
         <section className="user-content">
-          <div>
-            login/logout
+
+          <div className="user-profile">
+            <div className="user-sub-title">
+              프로필
+              <button>변경</button>
+            </div>
+            <div className="user-item">사진</div>
+            <div className="user-item">이름</div>
+            <div className="user-item">이메일</div>
+            <div className="user-item">설명</div>
           </div>
-          <div>
-            information
+
+          <div className="user-setting">
+            <div className="user-sub-title">
+              설정
+            </div>
+            <div className="user-item">
+              언어
+            </div>
+            <div className="user-item">
+              테마
+            </div>
           </div>
+
+          {/* <div className="user-join">
+            <div className="user-sub-title">
+              Login/Logout
+            </div>
+            <div className="user-item">
+              Id
+            </div>
+            <div className="user-item">
+              Password
+            </div>
+          </div> */}
+
         </section>
       </main>
     );
