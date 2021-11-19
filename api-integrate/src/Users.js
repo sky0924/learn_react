b3fe4dff7ab333cc93fe93de461671951ddaf1db
sky <sky0924>
2021-11-19 18:38:19 +0900

    update react project

--- api-integrate/src/Users.js
+++ api-integrate/src/Users.js
@@ -1,43 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOADING":
+      return {};
+    case "SUCCESS":
+      return {};
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
 function Users() {
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
+  const fetchUsers = async () => {
+    try {
+      setError(null);
+      setUsers(null);
+      setLoading(true);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUsers(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        setError(null);
-        setUsers(null);
-        setLoading(true);
-        const response = await axios.get(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        setUsers(response.data);
-      } catch (e) {
-        setError(e);
-      }
-
-      setLoading(false);
-    };
-
     fetchUsers();
   }, []);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error!</div>;
+  if (loading) return <h1> &nbsp; Loading... </h1>;
+  if (error) return <h1> &nbsp; Error! </h1>;
   if (!users) return null;
 
   return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.username} ({user.name})
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchUsers}>새로고침</button>
+    </>
   );
 }
 
