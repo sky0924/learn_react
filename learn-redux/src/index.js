44d4f221e5a7e810f947da79c3afcd9ab6e9092c
sky <sky0924>
2021-11-20 20:52:34 +0900

    learn react and redux

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -7,11 +7,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./modules";
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
-console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById("root")
